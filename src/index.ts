653350ddbba681dc79ee36ece5c298842e81db05
Heurtault Aubin <aub.heurtault@gmail.com>
2022-09-23 14:20:16 +0200

    création du serveur

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello World!");
+require('dotenv').config();
+
+import express from 'express';
+import {PORT} from './config/constants';
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => res.send("Hello world"));
+
+app.listen(PORT, ()=> {
+    console.log(`Server  is listening on port ${PORT}`)
+});
